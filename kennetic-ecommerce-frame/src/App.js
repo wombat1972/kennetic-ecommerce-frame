6b75d9100df2620a2485b2c4387dfe3f82bc1e37
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-01-02 20:44:41 -0500

    made head file

--- kennetic-ecommerce-frame/src/App.js
+++ kennetic-ecommerce-frame/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
 import data from './data';
+
+import Head from './Components/Head';
 
 function App() {
   const [products] = useState(data);
   return (
     <div className="App">
-      
+      <Head />
     </div>
   );
 }
---
+++ kennetic-ecommerce-frame/src/Components/Head.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Head() {
+    return(
+        <div>
+            <h1>TKB Custom Design</h1>
+        </div>
+    );
+}
+
+export default Head;
